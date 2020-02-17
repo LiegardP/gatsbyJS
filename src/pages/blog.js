26521f4cb08c289303dc52b9954ad14f683a01a0
LiegardP <pierre.liegard@gmail.com>
2020-02-17 23:05:38 +0100

    add post page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,22 +1,28 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Post from "../components/Post";
 import { graphql } from "gatsby";
 
 
 const Blog = ({data}) => (
   <Layout>
     <h1>Blog</h1>
-    <div>{ console.log(data) }</div>
-    <p>
-      Make it look like digital are we in agreeance, nor finance. Workflow 
-      ecosystem pivot one-sheet, yet if you could do that, that would be great. 
-      Quick win it's a simple lift and shift job draft policy ppml proposal and 
-      cross-pollination finance. Price point pull in ten extra bodies to help 
-      roll the tortoise. Drill down roll back strategy prioritize these line 
-      items so high turnaround rate. Pixel pushing killing it. Shoot me an email
-      teams were able to drive adoption and awareness. Throughput. Eat our own 
-      dog food how much bandwidth do you have goalposts or window of opportunity.
-    </p>
+    {
+      data.allMarkdownRemark.edges.map(post => {
+        const {title, author, date, description, path } = post.node.frontmatter;
+
+        return (
+          <Post
+            title={title}
+            author={author}
+            date={date}
+            description={description}
+            key={`${date}__${title}`}
+            path={path}
+          />
+        )
+      })
+    }
   </Layout>
 );
 export default Blog;
